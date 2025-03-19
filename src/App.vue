6f2c251c7620a89aa0aa853b165f30f995314f96
Beunador10 <beunadorsodji@gmail.com>
2025-03-19 01:39:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+import cours from './components/Programme_cours.vue'
+</script>
+
+<template>
+  <cours></cours>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Programme_cours.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Projet de gestion des programmes des cours</h1>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+</style>
