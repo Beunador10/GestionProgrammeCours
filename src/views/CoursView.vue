1332cefee6be668dc96b577fbeb988a750c176d1
Beunador10 <beunadorsodji@gmail.com>
2025-04-02 16:01:52 +0200

    dernière version MAJ

--- src/views/CoursView.vue
+++ src/views/CoursView.vue
@@ -5,14 +5,14 @@
         <h2 class="text-2xl font-bold">Cours</h2>
         <div class="flex items-center space-x-2">
           <input type="text" v-model="searchQuery" placeholder="Rechercher..." class="border p-2 rounded-md" />
-          <button @click="showForm = true" class="bg-blue-500 text-white px-4 py-2 rounded-md">+ Ajouter</button>
+          <button @click="showForm = true" class="bg-blue-500 text-white px-4 py-2 rounded-md"> <b class="text-xl font-bold">+</b> Ajouter</button>
         </div>
       </div>
-      
+
       <!-- Utilisation directe du store pour garantir la réactivité -->
       <CoursGrid :courses="coursStore.courses" :searchQuery="searchQuery" />
     </main>
-    
+
     <CoursForm v-if="showForm" @close="showForm = false" @add-course="handleAddCourse" />
   </div>
 </template>
